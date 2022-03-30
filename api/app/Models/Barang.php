ec4b04a93e63f3bca8725df1e82266d72563b447
ismarianto12 <kotokareh@gmail.com>
2022-03-30 17:20:05 +0700

    initial commit

---
+++ api/app/Models/Barang.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+
+    protected $table = 'barang';
+    public $incrementing = false;
+    public $guarded = [];
+}
